314c4e2f76742f28d9c09962c475b123a30cf3c4
RidhayNeeraj <ridhay1123@gmail.com>
2025-10-29 22:08:43 +0530

    fix: Enable Swagger in production

--- ProjectManager.Api/Program.cs
+++ ProjectManager.Api/Program.cs
@@ -51,10 +51,11 @@ var app = builder.Build();
 
 // --- Configure the HTTP request pipeline ---
 
+app.UseSwagger();
+app.UseSwaggerUI();
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    
 }
 
 app.UseCors("AllowReactApp");
